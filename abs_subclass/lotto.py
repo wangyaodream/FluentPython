c24192a69114600d9f0ffcaab96ff02c54822fb9
wangyao <wangyao.dream@hotmail.com>
2018-09-19 17:21:12 +0800

    add abs&subclass

---
+++ abs_subclass/lotto.py
@@ -0,0 +1,23 @@
+import random 
+from tombola import Tombola
+
+class LotteryBlower(Tombola):
+    def __init__(self,iterable):
+        self._balls = list(iterable)
+
+    def load(self,iterable):
+        self._balls.extend(iterable)
+
+    def pick(self)；
+        try:
+            position = random.randrange(len(self._balls))
+        except ValueError:
+            raise LookupError('pick from empty Lotteryblower')
+        return self._balls.pop(position)
+
+    def loaded(self):
+        return bool(self._balls)
+
+    def inspect(self):
+        return tuple(self._balls)
+        
